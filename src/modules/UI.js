4d1c03ccbbf488164603b167ee3025b19878cbf0
SynthCyraX <SynthCyraX@gmail.com>
2023-09-17 07:38:44 +0200

    Added 2 methods for separation of displayGrid

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -6,37 +6,69 @@ class UI {
     const computer = player2;
 
     // display boards in html/DOM
-    const humanBoard = player.gameboard.grid;
+    const playerBoard = player.gameboard.grid;
     const computerBoard = computer.gameboard.grid;
 
-    const grid = document.getElementById('grid');
+    UI.displayPlayerGrid(playerBoard);
+    UI.displayComputerGrid(computerBoard);
+  }
+
+  // take loop into separate function DRY
+  static displayPlayerGrid(playerBoard) {
+    const playerGrid = document.getElementById('playerGrid');
 
     for (let i = 0; i < 10; i += 1) {
       // create rows
-      // console.log(humanBoard[i]);
       const row = document.createElement('tr');
       for (let j = 0; j < 10; j += 1) {
         // create cells
         const cell = document.createElement('td');
         cell.setAttribute('id', `${i}${j}`);
 
-        if (humanBoard[i][j] !== 0) {
-          cell.innerHTML = humanBoard[i][j].value;
+        // if playerBoard[index][index] is !0, set innerHTML to .value of object
+        if (playerBoard[i][j] !== 0) {
+          cell.innerHTML = playerBoard[i][j].value;
+        }
+        // if playerBoard[index][index] is 0, set innerHtml to 0
+        if (playerBoard[i][j] === 0) {
+          cell.innerHTML = playerBoard[i][j];
         }
 
-        if (humanBoard[i][j] === 0) {
-          cell.innerHTML = humanBoard[i][j];
-        }
-
-        console.log(humanBoard[i][j]);
+        console.log(playerBoard[i][j]);
 
         row.append(cell);
       }
-      grid.append(row);
+      playerGrid.append(row);
     }
-    // console.log(humanBoard[0]);
   }
-  //
+
+  static displayComputerGrid(computerBoard) {
+    const computerGrid = document.getElementById('computerGrid');
+
+    for (let i = 0; i < 10; i += 1) {
+      // create rows
+      const row = document.createElement('tr');
+      for (let j = 0; j < 10; j += 1) {
+        // create cells
+        const cell = document.createElement('td');
+        cell.setAttribute('id', `${i}${j}`);
+
+        // if board[index][index] is !0, set innerHTML to .value of object
+        if (computerBoard[i][j] !== 0) {
+          cell.innerHTML = computerBoard[i][j].value;
+        }
+        // if board[index][index] is 0, set innerHtml to 0
+        if (computerBoard[i][j] === 0) {
+          cell.innerHTML = computerBoard[i][j];
+        }
+
+        console.log(computerBoard[i][j]);
+
+        row.append(cell);
+      }
+      computerGrid.append(row);
+    }
+  }
 }
 
 export default UI;
